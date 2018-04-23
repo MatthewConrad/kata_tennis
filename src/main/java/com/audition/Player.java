95e49ad7e92721596e0c7e16734c5d58f5ff03a6
Matthew Conrad <conrad.matthewj@gmail.com>
2018-04-22 22:58:08 -0400

    Fleshed out test cases. Added Player class.

---
+++ src/main/java/com/audition/Player.java
@@ -0,0 +1,55 @@
+package com.audition;
+
+/**
+ * Kata: Tennis
+ * The Kata uses simplified rules for Tennis.
+ * - Each player can have 0 ("love"), 15 ("fifteen"), 30 ("thirty"), or 40 ("forty") points
+ * - If a player has 40 points and wins another point, they win the game if they have scored at least
+ *   two more times than their opponent
+ * - If both players have 40 points, the game is in "deuce". The winner of the next point will have "advantage"
+ *   and must score another point in a row to win, but if the other player scores instead the game is back to deuce.
+ **/
+
+public class Player {
+
+    String name;
+    private int score;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public String getScoreDescription(){
+        String description;
+        switch (this.score){
+            case 0:
+                description = "love";
+                break;
+            case 1:
+                description = "fifteen";
+                break;
+            case 2:
+                description = "thirty";
+                break;
+            case 4:
+                description = "forty";
+                break;
+            default:
+                description = "";
+        }
+
+        return description;
+    }
+
+    public void scorePoint(){
+        this.score += 1;
+    }
+}
--- src/test/java/com/audition/TennisTest.java
+++ src/test/java/com/audition/TennisTest.java
@@ -2,66 +2,150 @@ package com.audition;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Kata: Tennis
+ * The Kata uses simplified rules for Tennis.
+ * - Each player can have 0 ("love"), 15 ("fifteen"), 30 ("thirty"), or 40 ("forty") points
+ * - If a player has 40 points and wins another point, they win the game if they have scored at least
+ *   two more times than their opponent
+ * - If both players have 40 points, the game is in "deuce". The winner of the next point will have "advantage"
+ *   and must score another point in a row to win, but if the other player scores instead the game is back to deuce.
+ **/
 
 public class TennisTest {
-
-    /**
-     * Kata: Tennis
-     * The Kata uses simplified rules for Tennis.
-     * - Each player can have 0 ("love"), 15 ("fifteen"), 30 ("thirty"), or 40 ("forty") points
-     * - If a player has 40 points and wins another point, they win the game if they have scored at least
-     *   two more times than their opponent
-     * - If both players have 40 points, the game is in "deuce". The winner of the next point will have "advantage"
-     *   and must score another point in a row to win, but if the other player scores instead the game is back to deuce.
-     **/
+    Player player1;
+    Player player2;
+    Game game;
 
     @Before
     public void beforeTennisTest(){
-
+        player1 = new Player("PlayerOne");
+        player2 = new Player("PlayerTwo");
+        game = new Game(player1, player2);
     }
 
     @Test
     public void initialScoreDescriptionShouldBeLove(){
-
+        assertEquals(game.getScore(), "love, love");
     }
 
     @Test
     public void onePointScoreDescriptionShouldBeFifteen(){
+        player1.scorePoint();
 
+        assertEquals(game.getScore(), "fifteen, love");
     }
 
     @Test
     public void twoPointsScoreDescriptionShouldBeThirty(){
+        int x = 0;
+        for(x = 0; x < 2; x++) {
+            player1.scorePoint();
+        }
 
+        assertEquals(game.getScore(), "thirty, love");
     }
 
     @Test
     public void threePointsScoreDescriptionShouldBeForty(){
+        int x = 0;
+        for(x = 0; x < 3; x++) {
+            player1.scorePoint();
+        }
 
+        assertEquals(game.getScore(), "forty, love");
     }
 
     @Test
-    public void firstPlayerToScoreFourTimesWithATwoPointLeadShouldWin(){
+    public void playersShouldScorePointsIndependently(){
+        player1.scorePoint();
+        assertEquals(game.getScore(), "fifteen, love");
 
+        player1.scorePoint();
+        assertEquals(game.getScore(), "thirty, love");
+
+        player2.scorePoint();
+        assertEquals(game.getScore(), "thirty, fifteen");
+
+        player2.scorePoint();
+        player2.scorePoint();
+        assertEquals(game.getScore(), "thirty, forty");
+    }
+
+    @Test
+    public void firstPlayerToScoreFourTimesWithAtLeastATwoPointLeadShouldWin(){
+        int x = 0;
+        for(x = 0; x < 4; x++) {
+            player1.scorePoint();
+        }
+
+        assertEquals(game.getScore(), "PlayerOne wins");
+    }
+
+    @Test
+    public void firstPlayerToScoreFourTimesWithExactlyATwoPointLeadShouldWin(){
+        int x = 0;
+        for(x = 0; x < 2; x++) {
+            player1.scorePoint();
+            player2.scorePoint();
+        }
+
+        player1.scorePoint();
+        player1.scorePoint();
+
+        assertEquals(game.getScore(), "PlayerOne wins");
     }
 
     @Test
     public void scoreDescriptionWhenPlayersTiedAtFortyShouldBeDeuce(){
+        int x = 0;
+        for(x = 0; x < 3; x++) {
+            player1.scorePoint();
+            player2.scorePoint();
+        }
 
+        assertEquals(game.getScore(), "deuce");
     }
 
     @Test
     public void scoreDescriptionWhenOnePlayerScoresAPointPastDeuceShouldBeAdvantage(){
+        int x = 0;
+        for(x = 0; x < 3; x++) {
+            player1.scorePoint();
+            player2.scorePoint();
+        }
 
+        assertEquals(game.getScore(), "deuce");
     }
 
     @Test
     public void scoreDescriptionWhenPlayerWithAdvantageFailsToScoreAgainShouldBeDeuce(){
+        int x = 0;
+        for(x = 0; x < 3; x++) {
+            player1.scorePoint();
+            player2.scorePoint();
+        }
+        player1.scorePoint();
 
+        assertEquals(game.getScore(), "advantage PlayerOne");
+        player2.scorePoint();
+        assertEquals(game.getScore(),"deuce");
     }
 
     @Test
     public void playerWithAdvantageShouldWinIfScoresAgainBeforeTheirOpponent() {
+        int x = 0;
+        for(x = 0; x < 3; x++) {
+            player1.scorePoint();
+            player2.scorePoint();
+        }
+        player1.scorePoint();
 
+        assertEquals(game.getScore(), "advantage PlayerOne");
+        player1.scorePoint();
+        assertEquals(game.getScore(),"PlayerOne wins");
     }
 }
